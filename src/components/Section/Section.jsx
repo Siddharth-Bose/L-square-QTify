c793e40a8ce872bc02e0e15252df1e8ddee5a57b
Siddharth Bose <siddharth.bose@iemlabs.com>
2025-04-16 00:51:36 +0530

    Fix No. of Cards Issue

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -5,7 +5,7 @@ import styles from "./Section.module.css";
 
 function Section({ title = "Top Albums", apiEndpoint }) {
   const [albums, setAlbums] = useState([]);
-  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(true);
   const scrollRef = useRef(null);
 
   useEffect(() => {
