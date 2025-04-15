e7297b5d6800e91c18e06f2a6ca6f06a592562aa
Siddharth Bose <siddharth.bose@iemlabs.com>
2025-04-15 22:48:12 +0530

    Add Navbar and Hero Section

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    include: [
+      '@mui/material',
+      '@mui/system',
+      '@mui/base',
+      '@mui/icons-material',
+    ],
+  },
+  resolve: {
+    dedupe: ['react', 'react-dom'], // Ensures React is not duplicated
+  },
+  build: {
+
+    /** If you set esmExternals to true, this plugins assumes that 
+      all external dependencies are ES modules */
+ 
+    commonjsOptions: {
+       esmExternals: true 
+    },
+ }
+});
